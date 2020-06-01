bb306eda1a860e82f46ff9fdb2100c15d999ac20
raythx98 <65889977+raythx98@users.noreply.github.com>
2020-06-02 01:06:26 +0800

    Some settings

--- app/src/main/java/com/example/powerpuffgirls/MainGameActivity.java
+++ app/src/main/java/com/example/powerpuffgirls/MainGameActivity.java
@@ -24,18 +24,13 @@ public class MainGameActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_flying_game);
 
-        try {
-                findViewById(R.id.play).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.play).setOnClickListener(new View.OnClickListener() {
 
-                    @Override
-                    public void onClick(View v) {
-                        startActivity(new Intent(MainGameActivity.this, GameActivity.class));
-                    }
-                });
-        } catch (Exception e) {
-            Log.d("25 @ Main Game Activity", e.getMessage());
-        }
-
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainGameActivity.this, GameActivity.class));
+            }
+        });
 
         TextView highScoreTxt = findViewById(R.id.highScoreTxt);
 
@@ -48,36 +43,28 @@ public class MainGameActivity extends AppCompatActivity {
 
         final ImageView volumeCtrl = findViewById(R.id.volumeCtrl);
 
-        try {
-            if (isMute) {
-                volumeCtrl.setImageResource(R.drawable.ic_baseline_volume_off_24);
-            } else {
-                volumeCtrl.setImageResource(R.drawable.ic_baseline_volume_up_24);
-            }
-        } catch (Exception e) {
-            Log.d("49 @ Main Game Activity", e.getMessage());
+
+        if (isMute) {
+            volumeCtrl.setImageResource(R.drawable.ic_baseline_volume_off_24);
+        } else {
+            volumeCtrl.setImageResource(R.drawable.ic_baseline_volume_up_24);
         }
 
-        try {
-            volumeCtrl.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
+        volumeCtrl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-                    isMute = !isMute;
-                    if (isMute) {
-                        volumeCtrl.setImageResource(R.drawable.ic_baseline_volume_off_24);
-                    } else {
-                        volumeCtrl.setImageResource(R.drawable.ic_baseline_volume_up_24);
-                    }
-                    SharedPreferences.Editor editor = prefs.edit();
-                    editor.putBoolean("isMute", isMute);
-                    editor.apply();
-
+                isMute = !isMute;
+                if (isMute) {
+                    volumeCtrl.setImageResource(R.drawable.ic_baseline_volume_off_24);
+                } else {
+                    volumeCtrl.setImageResource(R.drawable.ic_baseline_volume_up_24);
                 }
-            });
-        } catch (Exception e) {
-            Log.d("59 @ Main Game Activity", e.getMessage());
-        }
+                SharedPreferences.Editor editor = prefs.edit();
+                editor.putBoolean("isMute", isMute);
+                editor.apply();
 
+            }
+        });
     }
 }
