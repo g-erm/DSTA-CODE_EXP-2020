8132badd229b204dd4af7151933c294355563afe
ameliatjy <e0425671@u.nus.edu>
2020-06-02 19:34:29 +0800

    edit database structure

--- app/src/main/java/com/example/powerpuffgirls/FriendsActivity.java
+++ app/src/main/java/com/example/powerpuffgirls/FriendsActivity.java
@@ -1,14 +1,94 @@
 package com.example.powerpuffgirls;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.SearchView;
+import android.widget.ListView;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class FriendsActivity extends AppCompatActivity {
+    private static final String TAG = "FriendsActivity";
+
+    SearchView mySearchView;
+    ListView myList;
+
+    ArrayList<String> allId;
+    ArrayList<String> allNames = new ArrayList<>();
+    ArrayAdapter<String> adapter;
+
+    public HashMap<String, String> namesAndId = new HashMap<>();
+
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friends);
+
+        mySearchView = (SearchView)findViewById(R.id.searchView);
+        myList = (ListView)findViewById(R.id.friendsList);
+
+        mDatabase.child("names").addValueEventListener(new com.google.firebase.database.ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                getMap(dataSnapshot);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
+
+        if (!this.namesAndId.isEmpty()) {
+            this.allNames = new ArrayList<>(this.namesAndId.keySet());
+        }
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, this.allNames);
+        myList.setAdapter(adapter);
+
+        //String name = mySearchView.getQuery().toString();
+
+        mySearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+                adapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+    }
+
+    public void getMap(DataSnapshot dataSnapshot) {
+        HashMap<String, String> currmap = (HashMap<String, String>) dataSnapshot.getValue();
+        if (currmap != null) {
+            this.namesAndId = currmap;
+        } else {
+            this.namesAndId = new HashMap<>();
+        }
     }
 }
--- app/src/main/java/com/example/powerpuffgirls/LoginActivity.java
+++ app/src/main/java/com/example/powerpuffgirls/LoginActivity.java
@@ -1,11 +1,17 @@
 package com.example.powerpuffgirls;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
@@ -28,6 +34,8 @@ public class LoginActivity extends AppCompatActivity { //SafeDelete Type Paramet
     private FirebaseUser user;
     private DatabaseReference mDatabase;
     private MediaPlayer music;
+    private ArrayList<String> allId = new ArrayList<>();
+    public HashMap<String, String> namesAndId = new HashMap<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +44,7 @@ public class LoginActivity extends AppCompatActivity { //SafeDelete Type Paramet
 
         mAuth = FirebaseAuth.getInstance();
         mDatabase = FirebaseDatabase.getInstance().getReference();
+        getValuesFromDatabase();
         // database key: firebase user.userId, val: nric
     }
 
@@ -44,6 +53,7 @@ public class LoginActivity extends AppCompatActivity { //SafeDelete Type Paramet
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
+
         if (currentUser != null) {
             // onAuthSuccess(currentUser);
             startActivity(new Intent(LoginActivity.this, MenuActivity.class));
@@ -57,17 +67,18 @@ public class LoginActivity extends AppCompatActivity { //SafeDelete Type Paramet
         // updateUI(currentUser);
     }
 
-    private void createAccount (String email, final String password) {
+    private void createAccount (final String name, String email, final String password) {
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     private static final String TAG = "CreateUser";
+                    String user_name = name;
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "createUserWithEmail:success");
                             FirebaseUser user = mAuth.getCurrentUser();
-                            onAuthSuccess(task.getResult().getUser());
+                            onAuthSuccess(task.getResult().getUser(), user_name);
                             //updateUI(user);
                         } else {
                             // If sign in fails, display a message to the user.
@@ -82,18 +93,18 @@ public class LoginActivity extends AppCompatActivity { //SafeDelete Type Paramet
                 });
     }
 
-    public void signIn (String email, String password) {
+    public void signIn (final String name, String email, String password) {
         mAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     private static final String TAG = "Login";
-
+                    String user_name = name;
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "signInWithEmail:success");
                             FirebaseUser user = mAuth.getCurrentUser();
-                            onAuthSuccess(task.getResult().getUser());
+                            onAuthSuccess(task.getResult().getUser(), user_name);
                             startActivity(new Intent(LoginActivity.this, MenuActivity.class));
                             finish();
                             //pdateUI(user);
@@ -109,11 +120,12 @@ public class LoginActivity extends AppCompatActivity { //SafeDelete Type Paramet
     }
 
     public void Login(View view) {
+        String name = ((EditText)findViewById(R.id.name)).getText().toString();
         String NRIC = ((EditText)findViewById(R.id.NRIC)).getText().toString();
         NRIC += "@gmail.com";
         String password = ((EditText)findViewById(R.id.Password)).getText().toString();
         try {
-            signIn(NRIC, password);
+            signIn(name, NRIC, password);
         } catch (Exception e) {
             Toast.makeText(getApplicationContext(), "Invalud NRIC/Password",
                     Toast.LENGTH_SHORT).show();
@@ -121,11 +133,12 @@ public class LoginActivity extends AppCompatActivity { //SafeDelete Type Paramet
     }
 
     public void signUp(View view) {
+        String name = ((EditText)findViewById(R.id.name)).getText().toString();
         String NRIC = ((EditText)findViewById(R.id.NRIC)).getText().toString();
         NRIC += "@gmail.com";
         String password = ((EditText)findViewById(R.id.Password)).getText().toString();
         try {
-            createAccount(NRIC, password);
+            createAccount(name, NRIC, password);
         } catch (Exception e) {
             Toast.makeText(getApplicationContext(), "Invalid NRIC/Password",
                     Toast.LENGTH_SHORT).show();
@@ -140,15 +153,37 @@ public class LoginActivity extends AppCompatActivity { //SafeDelete Type Paramet
         }
     }
 
-    private void onAuthSuccess(FirebaseUser user) {
-        // Write new user
-        writeNewUser(user.getUid(), user.getEmail().substring(0, 9));
+    private void getValuesFromDatabase() {
+        mDatabase.addValueEventListener(new com.google.firebase.database.ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                getExistingMap(dataSnapshot);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+            }
+        });
     }
 
-    private void writeNewUser(String userId, String nric) {
+    private void getExistingMap(DataSnapshot dataSnapshot) {
+        this.namesAndId = (HashMap<String, String>) dataSnapshot.child("names").getValue();
+    }
+
+    private void onAuthSuccess(FirebaseUser user, String name) {
+        // Write new user
+        writeNewUser(user.getUid(), user.getEmail().substring(0, 9), name);
+    }
+
+    private void writeNewUser(String userId, String nric, String name) {
         //User user = new User(nric);
 
-        mDatabase.child("users").child(userId).child("nric").setValue(nric);
+        if (this.namesAndId == null) { this.namesAndId = new HashMap<>(); }
+        this.namesAndId.put(name, userId);
+
+        mDatabase.child("names").setValue(this.namesAndId);
+        mDatabase.child("users").child(userId).child("profile").child("nric").setValue(nric);
+        mDatabase.child("users").child(userId).child("profile").child("name").setValue(name);
     }
 
 }
