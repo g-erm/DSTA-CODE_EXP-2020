7d6d6b6fa55384305422e880e66ab588187aaae8
raythx98 <65889977+raythx98@users.noreply.github.com>
2020-06-03 21:56:51 +0800

    Travel declaration does not overwrite database, added health declaration (daily)

--- app/src/main/java/com/example/powerpuffgirls/DeclarationActivity.java
+++ app/src/main/java/com/example/powerpuffgirls/DeclarationActivity.java
@@ -13,6 +13,9 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class DeclarationActivity extends AppCompatActivity {
 
     private Spinner countryChoices;
@@ -35,7 +38,7 @@ public class DeclarationActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         countryChoices.setAdapter(adapter);
 
-        Button submit = findViewById(R.id.submit);
+        Button submit = findViewById(R.id.submitTravel);
         final EditText name = findViewById(R.id.fullname);
         final EditText departure = findViewById(R.id.departure);
         final EditText returnDate = findViewById(R.id.returnDate);
@@ -45,14 +48,32 @@ public class DeclarationActivity extends AppCompatActivity {
                 String departureString = departure.getText().toString();
                 String returnString = returnDate.getText().toString();
 
-                mDatabase.child("users").child(mAuth.getUid()).child("travel").child("country").setValue(countryChoices.getSelectedItem().toString());
-                mDatabase.child("users").child(mAuth.getUid()).child("travel").child("departure").setValue(departureString);
-                mDatabase.child("users").child(mAuth.getUid()).child("travel").child("return").setValue(returnString);
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy @ hh-mm-ss");
+                String format = simpleDateFormat.format(new Date());
+
+                mDatabase.child("users").child(mAuth.getUid()).child("travel").child(format).child("country").setValue(countryChoices.getSelectedItem().toString());
+                mDatabase.child("users").child(mAuth.getUid()).child("travel").child(format).child("departure").setValue(departureString);
+                mDatabase.child("users").child(mAuth.getUid()).child("travel").child(format).child("return").setValue(returnString);
 
                 name.setText("");
                 departure.setText("");
                 returnDate.setText("");
                 countryChoices.setAdapter(adapter);
+            }
+        });
+
+        Button submit2 = findViewById(R.id.submitHealth);
+        final EditText temperature = findViewById(R.id.tempInput);
+        submit2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String temp = temperature.getText().toString();
+                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd-MM-yyyy");
+                String format = simpleDateFormat.format(new Date());
+
+                mDatabase.child("users").child(mAuth.getUid()).child("health").child(format).child("temperature").setValue(temp);
+
+                temperature.setText("");
             }
         });
     }
